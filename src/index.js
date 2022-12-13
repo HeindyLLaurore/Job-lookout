e951545ef961091b51907cdcd966b5232daf7ffe
HeindyLLaurore <95661415+HeindyLLaurore@users.noreply.github.com>
2022-12-13 12:54:38 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "normalize.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./context/appContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
